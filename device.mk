a6486c8ed9e2f474980314c40932db5380c38431
Pranav Vashi <neobuddy89@gmail.com>
2021-03-31 22:18:49 +0530

    Phoenix: Add hotword enrollment
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: StarkDroid <gamerprince.exp@gmail.com>

--- device.mk
+++ device.mk
@@ -12,6 +12,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
 # Get non-open-source specific aspects
+$(call inherit-product, vendor/xiaomi/phoenix/google/google.mk)
 $(call inherit-product, vendor/xiaomi/phoenix/phoenix-vendor.mk)
 
 # Include PSU if synced
@@ -70,7 +71,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@5.0-impl \
     android.hardware.audio@6.0 \
     android.hardware.audio@6.0-impl \
-    android.hardware.soundtrigger@2.2-impl:32 \
     audio.a2dp.default \
     audio.primary.sm6150:32 \
     audio.r_submix.default \
